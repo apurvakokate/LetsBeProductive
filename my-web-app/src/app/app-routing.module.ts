e56cd29875e3f19ba0fe2902ec228e1fc29d7551
Apurva Kokate <apurvakokate94@gmail.com>
2019-08-20 16:10:05 -0500

    routing module setup and footer changes

--- my-web-app/src/app/app-routing.module.ts
+++ my-web-app/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
+import {CodingComponent} from "./coding/coding.component";
+import {ResearchComponent} from "./research/research.component";
+import {PoetryComponent} from "./poetry/poetry.component";
+import {NewIdeasComponent} from "./new-ideas/new-ideas.component";
+import {LifeExperiencesComponent} from "./life-experiences/life-experiences.component";
+import {ContactComponent} from "./contact/contact.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  {path: 'welcome', component: WelcomePageComponent},
+  {path: 'coding', component: CodingComponent},
+  {path: 'research', component: ResearchComponent},
+  {path: 'poetry', component: PoetryComponent},
+  {path: 'ideas', component: NewIdeasComponent},
+  {path: 'life', component: LifeExperiencesComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

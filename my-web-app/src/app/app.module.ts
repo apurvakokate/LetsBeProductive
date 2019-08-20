2eacafb656cfa694d8501107cecf2dc486936ed9
Apurva Kokate <apurvakokate94@gmail.com>
2019-08-20 14:56:38 -0500

    welcome page component

--- my-web-app/src/app/app.module.ts
+++ my-web-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
